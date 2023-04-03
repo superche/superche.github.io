db75cd97caaf9d0ddd3305f6b0e9de124d9e722c
superche <shawche@outlook.com>
2023-04-03 19:42:52 +0800

    fix: robots.txt issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import { settings } from './src/data/settings';
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: settings.site,
-  integrations: [sitemap(), mdx()],
+  integrations: [sitemap(), robotsTxt(), mdx()],
   vite: {
     ssr: {
       external: ["svgo"]
